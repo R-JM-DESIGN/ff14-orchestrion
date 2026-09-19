aff7d22e31434ca98e985f351e2ba7a75ec0e4b9
JM_Design <150700144+R-JM-DESIGN@users.noreply.github.com>
2026-09-19 18:15:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // app.js - Part 1 (순수 6열 스키마 전진 매핑 및 원천 최적화)
 // 🌟 사용자님의 구글 웹 앱 API 주소를 상단에 고정하여 초고속 연동을 지원합니다.
 // =========================================================================
-const GOOGLE_WEB_APP_URL = 'https://google.com';
+const GOOGLE_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyLffvGl00EPd7ZgcUF0nQv3uU39HfoIo5mFPIlk7_x8LK94ygFkhRnAUfSccrG_aja9w/exec';
 const SHEET_URL = GOOGLE_WEB_APP_URL; 
 
 let rawData = [];
