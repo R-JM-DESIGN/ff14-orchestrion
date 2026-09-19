eba796701629cf0f6c1fcbc5469e404ba2d60ab4
JM_Design <150700144+R-JM-DESIGN@users.noreply.github.com>
2026-09-19 18:41:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // app.js - Part 1 (순수 6열 스키마 전진 매핑 및 구글 무한 타임아웃 격파 버전)
 // 🌟 사용자님의 구글 웹 앱 API 주소를 상단에 고정하여 초고속 연동을 지원합니다.
 // =========================================================================
-const GOOGLE_WEB_APP_URL = 'https://google.com';
+const GOOGLE_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzva7HH9j9XnYLxk4jfkgOxSbVCaHKnqk_tGyiKX4FpbHNfruGkWYp2fBjv8s4O-o3DSw/exec';
 const SHEET_URL = GOOGLE_WEB_APP_URL; 
 
 let rawData = [];
